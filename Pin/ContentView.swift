e9f8ce813306d203138f42611278857c24043652
catherine <tsailingyu@pm.me>
2024-04-02 19:53:26 -0400

    Initial Commit

---
+++ Pin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pin
+//
+//  Created by adobada on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pin/PinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinApp.swift
+//  Pin
+//
+//  Created by adobada on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
